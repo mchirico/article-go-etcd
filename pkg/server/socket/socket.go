cfd62c2f86a5fb5635077505240c438c1ab5c014
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-21 12:09:40 -0500

    update-automated-save

---
+++ pkg/server/socket/socket.go
@@ -0,0 +1,59 @@
+package socket
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func Example() {
+	messages := []string{}
+
+
+	server,_ := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		for _,v := range messages {
+			s.Emit("reply", "...."+v)
+		}
+		s.Emit("reply", "have "+msg)
+		
+	})
+
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		messages = append(messages, msg)
+		return "recv " + msg
+	})
+
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
